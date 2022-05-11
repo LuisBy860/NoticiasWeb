201ad7e0311c53c88e66c45fdd67ee151c16e947
Jose101503 <ph0172032019@unab.edu.sv>
2022-05-11 15:17:14 -0600

    Terminada Suscripciones Crud
    Listo

--- NoticiasWeb/Controllers/SuscripcionController.cs
+++ NoticiasWeb/Controllers/SuscripcionController.cs
@@ -1,12 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using NoticiasWeb.Data;
+using NoticiasWeb.Models;
+using NoticiasWeb.Service;
+
+using Microsoft.AspNetCore.Mvc;
+using NoticiasWeb.Repository;
 
 namespace NoticiasWeb.Controllers
 {
     public class SuscripcionController : Controller
     {
+        private ISuscripcion Suscripcion;
+
+        public SuscripcionController(ISuscripcion suscripcion)
+        {
+            Suscripcion = suscripcion;
+        }
+
+      
+        public IActionResult actu(Models.Suscripciónes su)
+        {
+
+            Suscripcion.UpdateSuscripciones(su);
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult Guarda(Models.Suscripciónes su)
+        {
+            Suscripcion.AddSuscripciones(su);
+
+            return Redirect("Index");
+        }
+        public IActionResult Agregar()
+        {
+
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            Suscripciónes su = new Suscripciónes();
+            su.SuscripcionId = id;
+            Suscripcion.DeleteSuscripciones(su);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Load(int id)
+        {
+            Suscripciónes su = new Suscripciónes();
+            su.SuscripcionId = id;
+            var listarsuscripcion = Suscripcion.LoadInformation(su);
+
+            return View(listarsuscripcion);
+        }
         public IActionResult Index()
         {
-            return View();
+
+            var listar = Suscripcion.Getall();
+
+            return View(listar);
+        }
+        [HttpPost]
+        public IActionResult Insertardata(Suscripciónes ca)
+        {
+
+            return RedirectToAction("Index");
+            // return View("Index");
+
         }
     }
 }
