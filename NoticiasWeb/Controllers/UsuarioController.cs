6471e9bd6c0f8d8647e87a35d88b5f9e6dbfe1c0
Gersson Leon <ll0204032017@unab.edu.sv>
2022-05-29 10:21:38 -0600

    Revert "Revert "disenoLogin""
    This reverts commit 5a4e9c6d580744fb1cd96a9996b949d9d8dcf19c.

--- NoticiasWeb/Controllers/UsuarioController.cs
+++ NoticiasWeb/Controllers/UsuarioController.cs
@@ -7,62 +7,65 @@ namespace NoticiasWeb.Controllers
     public class UsuarioController : Controller
     {
 
-            private IUsuario Usuario;
+        private IUsuario Usuario;
 
-            public UsuarioController(IUsuario usuario)
-            {
-                Usuario = usuario;
-            }
+        public UsuarioController(IUsuario usuario)
+        {
+            Usuario = usuario;
+        }
 
 
-            public IActionResult actu(Usuarios us)
-            {
+        public IActionResult actu(Usuarios us)
+        {
 
             Usuario.UpdateUsuarios(us);
-                return RedirectToAction("Index");
+            return RedirectToAction("Index");
 
-            }
-            public IActionResult Guarda(Usuarios us)
-            {
-                Usuario.AddUsuarios(us);
-
-                return Redirect("Index");
-            }
-            public IActionResult Agregar()
-            {
-
-                return View();
-            }
-            public IActionResult Delete(int id)
-            {
-                Usuarios us = new Usuarios();
-                us.UsuarioId = id;
-                Usuario.DeleteUsuarios( us);
-
-                return RedirectToAction("Index");
-            }
-            public IActionResult Load(int id)
-            {
-                Usuarios us = new ();
-                us.UsuarioId = id;
-                var listarusuario = Usuario.LoadInformation(us);
-
-                return View(listarusuario);
-            }
-            public IActionResult Index()
-            {
-
-                var listar = Usuario.Getall();
-
-                return View(listar);
-            }
-            [HttpPost]
-            public IActionResult Insertardata(Usuarios us)
-            {
-
-                return RedirectToAction("Index");
-                // return View("Index");
-
-            }
         }
+        public IActionResult Guarda(Usuarios us)
+        {
+            //estos serian los datos por defecto en privilegios y suscripcion a la hora de registrar un usuario
+            us.PrivilegioID = 1;
+            us.SuscripcionID = 1;
+            Usuario.AddUsuarios(us);
+;
+            return Redirect("Index");
+        }
+        public IActionResult Agregar()
+        {
+
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            Usuarios us = new Usuarios();
+            us.UsuarioId = id;
+            Usuario.DeleteUsuarios(us);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Load(int id)
+        {
+            Usuarios us = new();
+            us.UsuarioId = id;
+            var listarusuario = Usuario.LoadInformation(us);
+
+            return View(listarusuario);
+        }
+        public IActionResult Index()
+        {
+
+            var listar = Usuario.Getall();
+
+            return View(listar);
+        }
+        [HttpPost]
+        public IActionResult Insertardata(Usuarios us)
+        {
+
+            return RedirectToAction("Index");
+            // return View("Index");
+
+        }
+    }
 }
